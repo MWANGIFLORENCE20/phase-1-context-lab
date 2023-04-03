01c618ba52692fdb64a3c817b0d39e98e4742b43
florence wangechi <florencewangechim@gmail.com>
2023-04-03 14:31:36 +0300

    Compled

--- index.js
+++ index.js
@@ -1,4 +1,68 @@
 /* Your Code Here */
+const createEmployeeRecord = (data) => {
+    return {
+        firstName: data[0],
+        familyName: data[1],
+        title:data[2],
+        payPerHour: data[3],
+        timeInEvents:[],
+        timeOutEvents: [], 
+    }
+};
+
+const createEmployeeRecords = arrays => {
+    return arrays.map(createEmployeeRecord)
+}
+
+const createTimeInEvent = function(dateStamp){
+    this.timeInEvents.push({
+        type:"TimeIn",
+        date:dateStamp.split("")[0],
+        hour: parseInt(dateStamp.split("")[1])
+    })
+
+    return this
+}
+
+const createTimeOutEvent = function(dateStamp){
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        date: dateStamp.split("")[0],
+        hour: parseInt(dateStamp.split("")[1])
+    })
+    return this
+}
+
+const hoursWorkedOnDate = function(date){
+    let timeIn = this.timeInEvents.find(e => e.date === date)
+    let timeOut = this.timeOutEvents.find(e => e.date ===date)
+    return (timeOut.hour - timeIn.hour)/100
+}
+
+const wagesEarnedOnDate = function (date){
+    return hoursWorkedOnDate.call(this,date)*this.payPerHour
+}
+const findEmployeeByFirstName = (array, name) => {
+    return array.find(rec => rec.firstName === name)
+}
+
+let calculutePayroll = function(employeeRec){
+    return employeeRec.reduce((memo, rec)=> memo + allWagesFor.call(rec),-0)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
